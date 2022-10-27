e9c95fe7b7e821dd73a372d4b8db59e57a5b46a8
dunyuchen <dunyuchen@live.com>
2022-10-27 01:12:32 +0100

    create signal generation module

--- muscle_sim/signal_gen.py
+++ muscle_sim/signal_gen.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import plot
 import numpy as np
 
 class signal_sin:
@@ -42,6 +43,15 @@ class signal_sin:
         self.output = self.center + self.amp * (np.sin(self.w * self.t_global))
         return self.output
 
+    def get_t(self):
+        """
+        acquire the current global time of the signal
+
+        returns:
+            self.t_global: current global time 
+        """
+        return self.t_global
+
     def search(self,T_global):
         """
         search the signal value from a given global time
@@ -55,3 +65,41 @@ class signal_sin:
         result = self.center + self.amp * (np.sin(self.w * T_global))
         return result
 
+# test 
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    # define the parameters
+    t_total = 5.0  # 5s in realtime
+    t_sample = 0.01
+    N_iter = int(t_total // t_sample) 
+
+    # initialization
+
+    signal = signal_sin(
+        Amp= 1,
+        Period= 2.5,
+        Center= 0,
+        T_sample=t_sample
+    )
+
+    plotMemory_output = np.zeros([N_iter, 1])
+    plotMemory_time = np.zeros([N_iter, 1])
+
+    for iter in range(N_iter):
+        plotMemory_output[iter] = signal.step()
+        plotMemory_time[iter] = signal.get_t()
+
+    #plot
+
+    #fig =plt.figure()
+    #a=fig.add_axes([0,0,5,1])
+    #a.plot(plotMemory_time,plotMemory_output)
+    # plot
+    fig, a = plt.subplots(1, 1)
+    a.set_title("Displacement")
+    a.set_xlabel("t - s")
+    a.set_ylabel("y - mm")
+    a.plot(plotMemory_time, plotMemory_output)
+    a.grid(True)
+    plt.show()
